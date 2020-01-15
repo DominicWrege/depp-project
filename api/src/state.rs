f3b2f46d938311af9c22faa6bd8db88149e2a0a6
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-01-16 00:01:56 +0100

    try to store credentials using sha2

--- api/src/state.rs
+++ api/src/state.rs
@@ -1,7 +1,12 @@
 use crate::api::IliasId;
 use grpc_api::test_client::TestClient;
+use serde::{Deserialize, Serialize};
+use sha2::Digest;
+use std::convert::TryInto;
 use std::ops::{Deref, DerefMut};
+use std::path::Path;
 use std::sync::Arc;
+use tokio::fs;
 use url::Url;
 
 #[derive(Clone)]
@@ -18,12 +23,61 @@ pub struct RpcConfig {
     rpc_url: Url,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Sha256(#[serde(with = "hex_serde")] pub [u8; 32]);
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct Credentials {
+    username: String,
+    password: Sha256,
+}
+
+impl Credentials {
+    pub fn new(username: String, pwd: Sha256) -> Credentials {
+        Credentials {
+            username: username,
+            password: pwd,
+        }
+    }
+
+    pub fn username(&self) -> &str {
+        &self.username
+    }
+    pub fn password(&self) -> Vec<u8> {
+       self.password.0.to_vec()
+    }
+}
+
+impl Default for Credentials{
+    fn default() -> Self {
+        let mut haser = sha2::Sha256::new();
+        haser.input("wasd");
+        let result = haser.result().try_into().unwrap();
+        Credentials::new("user".into(), Sha256(result) )
+    }
+}
+
+pub async fn get_credentials() -> Result<Credentials, tokio::io::Error> {
+    let file_path = Path::new("./credentials_api.toml");
+    if !file_path.exists() {
+        fs::File::create(file_path).await?;
+        let cred = Credentials::default();
+        fs::write(&file_path, toml::to_string(&cred).unwrap()).await?;
+        Ok(cred)
+    }else{
+        let file_content = fs::read_to_string(file_path).await?;
+        let cred = toml::from_str::<Credentials>(&file_content)?;
+        Ok(cred)
+    }
+}
+
 impl State {
-    pub fn new(rpc_conf: RpcConfig) -> State {
+    pub fn new(rpc_conf: RpcConfig, credentials: Credentials) -> State {
         State {
             inner: Arc::new(InnerState {
                 pending_results: dashmap::DashMap::new(),
                 rpc_url: rpc_conf.rpc_url,
+                credentials,
             }),
         }
     }
@@ -59,6 +113,7 @@ pub enum EndPointStatus {
 pub struct InnerState {
     pub pending_results: dashmap::DashMap<IliasId, grpc_api::AssignmentResult>,
     pub rpc_url: Url,
+    pub credentials: Credentials,
 }
 
 impl Deref for State {
