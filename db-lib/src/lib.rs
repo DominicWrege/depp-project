fbbd896bf40d8b36cfe345f49fcf1519ea95b6b4
DomincWrege <dominic.wrege001@stud.fh-dortmund.de>
2020-03-22 20:02:22 +0100

    fixes for testing

--- db-lib/src/lib.rs
+++ db-lib/src/lib.rs
@@ -1,11 +1,10 @@
 use deadpool_postgres::{Manager, Pool};
-use failure::ResultExt;
 
 mod embedded {
     use refinery::embed_migrations;
     embed_migrations!("./db-lib/migrations");
 }
-
+// TODO refactor conn err
 #[derive(Debug, err_derive::Error, derive_more::From)]
 pub enum DbError {
     #[error(display = "{}", _0)]
@@ -14,25 +13,32 @@ pub enum DbError {
     TypeMap(tokio_pg_mapper::Error),
     #[error(display = "Sql error {:?}", _0)]
     Sql(tokio_postgres::error::Error),
-    #[error(display = "DB returned empty rows")]
-    EmptyRows,
+    #[error(display = "Config error {}", _0)]
+    Config(ConfigError),
+}
+#[derive(Debug, err_derive::Error)]
+pub enum ConfigError {
+    #[error(display = "Connection failed using config {:#?}. Error: {}", _0, _1)]
+    Connection(DbConfig, tokio_postgres::error::Error),
+    #[error(display = "Migration failed: {}", _0)]
+    Migration(refinery_migrations::Error),
 }
 
 // user for testing
 //pub const DB_URL: &'static str = "postgres://john:12345@127.0.0.1:5432/assignments";
 
-#[derive(serde::Deserialize, Debug)]
-pub(crate) struct DbConfig {
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct DbConfig {
     user: String,
     password: String,
     #[serde(default = "default_db_name")]
-    name: String,
+    db: String,
     #[serde(default = "default_port")]
     port: u16,
     #[serde(default = "default_max_connection")]
     max_connection: usize,
     #[serde(default = "default_host")]
-    db_host: String,
+    host: String,
 }
 
 fn default_max_connection() -> usize {
@@ -55,42 +61,40 @@ impl Default for DbConfig {
         DbConfig {
             user: "john".into(),
             password: "12345".into(),
-            name: default_db_name(),
+            db: default_db_name(),
             port: default_port(),
             max_connection: default_max_connection(),
-            db_host: default_host(),
+            host: default_host(),
         }
     }
 }
 
 pub(crate) fn get_db_config() -> DbConfig {
-    match envy::from_env::<DbConfig>() {
+    match envy::prefixed("POSTGRES_").from_env::<DbConfig>() {
         Ok(config) => config,
         Err(_) => DbConfig::default(),
     }
 }
 
-pub async fn connect_migrate() -> Result<Pool, failure::Error> {
+pub async fn connect_migrate() -> Result<Pool, DbError> {
     let env_conf = get_db_config();
     let mut pg_config = tokio_postgres::Config::default();
     pg_config
         .user(&env_conf.user)
         .password(&env_conf.password)
-        .dbname(&env_conf.name)
-        .host(&env_conf.db_host)
+        .dbname(&env_conf.db)
+        .host(&env_conf.host)
         .port(env_conf.port);
 
     let (mut client, pg) = pg_config
         .connect(tokio_postgres::NoTls)
         .await
-        .context(format!(
-            "DB config error. Tried to connect to host: {}." & env_conf.db_host
-        ))?;
+        .map_err(|e| ConfigError::Connection(env_conf.clone(), e))?;
     tokio::task::spawn(pg);
-    let _ = embedded::migrations::runner()
+    embedded::migrations::runner()
         .run_async(&mut client)
         .await
-        .context("DB migration failed.")?;
+        .map_err(|e| ConfigError::Migration(e))?;
     let mngr = Manager::new(pg_config.clone(), tokio_postgres::NoTls);
     Ok(Pool::new(mngr, env_conf.max_connection))
 }
